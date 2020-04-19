66136a5450c6c1abea28dcabf70addf9198aed7b
Gert Stalpaert <gertst@gmail.com>
2020-04-19 15:13:55 +0200

    Cleanup

--- info/utils.js
+++ info/utils.js
@@ -3,7 +3,7 @@
 let Utils = {
 	bitAtGivenPosSetOrUnset(binary, position) {
 		const new_num = binary >> (position - 1);
-		console.log("set bin pos:", new_num, binary, position);
+		//console.log("set bin pos:", new_num, binary, position);
 		// if it results to true then bit is set,
 		// else it results to false bit is unset
 		//console.log("1/0:", new_num & 1);
